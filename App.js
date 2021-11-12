3b7152d862a2f5cfc5c9cedcc0d62dae785071ea
gywgithub <qingyi_w@outlook.com>
2021-11-12 14:55:02 +0800

    add react-native-mmkv example

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { simpleUpdate } from 'react-native-update';
 import { View, Text, Image, ScrollView, TextInput } from 'react-native';
+import { MMKV } from 'react-native-mmkv'
+
+// --- 以下代码不能在远程 debbuger 模式下运行 ---
+const storage = new MMKV()
+storage.set('user.name', 'Marc')
+storage.set('user.age', 21)
+const username = storage.getString('user.name') // 'Marc'
+const age = storage.getNumber('user.age') // 21
+console.log(username)
+console.log(age)
+// --- end ---
 
 const App = () => {
   return (
     <ScrollView>
       <Text>123456</Text>
-      <Text>abc</Text>      
+      <Text>abc</Text>
       <View>
         <Text>some more text</Text>
         <Image
