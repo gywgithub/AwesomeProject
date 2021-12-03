06c4160de3cc42959b2d302468a33c4ae63ca492
gywgithub <qingyi_w@outlook.com>
2021-12-03 17:59:31 +0800

    add progress, images, pages folder

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { simpleUpdate } from 'react-native-update';
 import { View, Text, Image, ScrollView, TextInput } from 'react-native';
 import { MMKV } from 'react-native-mmkv'
+import * as Progress from 'react-native-progress';
+
 
 // --- 以下代码不能在远程 debbuger 模式下运行 ---
 const storage = new MMKV()
@@ -31,6 +33,11 @@ const App = () => {
           borderColor: 'gray',
           borderWidth: 1
         }} defaultValue="You can type in me" />
+        <Progress.Bar
+          progress={0.2}
+          width={null}
+          color={'#30B7FF'}
+        />
       </View>
     </ScrollView>
   );
